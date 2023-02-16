212128d83e417390d5a57f01b771dbc307a80cb1
Isabela Silva <isabelapaivat15@gmail.com>
2023-02-16 11:12:20 -0300

    Aprendendo a diferença entre variáveis e constantes

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -2,9 +2,20 @@ public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         double salarioMinimo = 2500;
 
-        short numeroCurto = 1;
-        int numeroNormal = numeroCurto;
-        short numeroCurto2 = (short) numeroNormal;
+            short numeroCurto = 1;
+            int numeroNormal = numeroCurto;
+            short numeroCurto2 = (short) numeroNormal;
 
+        int numero = 5;//variável 
+
+        numero = 10;//valor da variável mudou 
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14; //final define constante caixa alta para nomear a constante.
+
+    
+
+    
     }
 }
